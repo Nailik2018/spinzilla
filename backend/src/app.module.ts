ad27c0f898cc6d7c804584f1104851ff664b11fe
= <kilualthaus@hotmail.com>
2024-10-06 14:07:48 +0200

    beispiel entfernen

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,4 @@
 import {Module} from '@nestjs/common';
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
 import {ConfigModule, ConfigService} from '@nestjs/config';
 import {TypeOrmModule} from '@nestjs/typeorm';
 
@@ -24,8 +22,8 @@ import {TypeOrmModule} from '@nestjs/typeorm';
         inject: [ConfigService],
       }),
     ],
-    controllers: [AppController],
-    providers: [AppService],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {
 }

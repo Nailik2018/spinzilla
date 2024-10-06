5a3724eb33d4d545fdbca7751e29634ddaca23ce
= <kilualthaus@hotmail.com>
2024-10-06 14:06:00 +0200

    intergration swagger, typeorm und nest config service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,31 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {ConfigModule, ConfigService} from '@nestjs/config';
+import {TypeOrmModule} from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+      ConfigModule.forRoot({
+        isGlobal: true,
+      }),
+      TypeOrmModule.forRootAsync({
+        imports: [ConfigModule],
+        useFactory: (configService: ConfigService) => ({
+          type: 'mysql',
+          host: configService.get<string>('MYSQL_HOST'),
+          port: configService.get<number>('MYSQL_PORT'),
+          username: configService.get<string>('MYSQL_USER'),
+          password: configService.get<string>('MYSQL_PASSWORD'),
+          database: configService.get<string>('MYSQL_DATABASE'),
+          entities: [__dirname + "/**/*.entity{.ts,.js}"],
+          synchronize: true
+        }),
+        inject: [ConfigService],
+      }),
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,37 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+import {DocumentBuilder, SwaggerModule} from '@nestjs/swagger';
+import {VersioningType} from '@nestjs/common';
+import {ConfigService} from '@nestjs/config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+
+    const configService = app.get(ConfigService);
+
+    // @ToDo bleibt in der Entwicklungsphase (Frontend Entwicklung) drin Cors erlauben
+    app.enableCors();
+    // api url prefix
+    app.setGlobalPrefix('api');
+    // versioning
+    app.enableVersioning({
+        type: VersioningType.URI,
+    });
+
+    const config = new DocumentBuilder()
+        .addBearerAuth()
+        .setTitle('Spinzilla')
+        .setDescription('Table Tennis Backend API System')
+        .setVersion('1.0.0')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup(process.env.BACKEND_SWAGGER_PATH, app, document);
+    const NODE_ENV = process.env.NODE_ENV || 'development';
+    // const PORT: number = Number(process.env.BACKEND_PORT) || 8080;
+    const PORT: number = Number(process.env.BACKEND_PORT) || 3000;
+    await app.listen(PORT, () => {
+        console.log(`Running NestJS Backend API Mode: ${NODE_ENV} Port: ${PORT}`);
+    });
 }
+
 bootstrap();

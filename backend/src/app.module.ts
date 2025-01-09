d554b3e79be907926c9656060119ab6bf6dfddce
= <kilualthaus@hotmail.com>
2025-01-09 19:43:46 +0100

    vercel pg test

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -13,17 +13,32 @@ import {ClubModule} from './models/club/club.module';
         ConfigModule.forRoot({
             isGlobal: true,
         }),
+        // TypeOrmModule.forRootAsync({
+        //     imports: [ConfigModule],
+        //     useFactory: (configService: ConfigService) => ({
+        //         type: 'mysql',
+        //         host: configService.get<string>('MYSQL_HOST'),
+        //         port: configService.get<number>('MYSQL_PORT'),
+        //         username: configService.get<string>('MYSQL_USER'),
+        //         password: configService.get<string>('MYSQL_PASSWORD'),
+        //         database: configService.get<string>('MYSQL_DATABASE'),
+        //         entities: [__dirname + "/**/*.entity{.ts,.js}"],
+        //         synchronize: true
+        //     }),
+        //     inject: [ConfigService],
+        // }),
         TypeOrmModule.forRootAsync({
             imports: [ConfigModule],
             useFactory: (configService: ConfigService) => ({
-                type: 'mysql',
-                host: configService.get<string>('POSTGRES_HOST'),
-                port: 3306,
-                username: configService.get<string>('POSTGRES_USER'),
-                password: configService.get<string>('POSTGRES_PASSWORD'),
-                database: configService.get<string>('POSTGRES_DATABASE'),
+                type: 'postgres', // Ändere den Datenbank-Typ
+                host: configService.get<string>('POSTGRES_HOST'), // Neon Host
+                port: configService.get<number>('POSTGRES_PORT') || 5432, // PostgreSQL-Port
+                username: configService.get<string>('POSTGRES_USER'), // Benutzername
+                password: configService.get<string>('POSTGRES_PASSWORD'), // Passwort
+                database: configService.get<string>('POSTGRES_DATABASE'), // Datenbankname
+                ssl: { rejectUnauthorized: false }, // Neon benötigt SSL
                 entities: [__dirname + "/**/*.entity{.ts,.js}"],
-                synchronize: true
+                synchronize: true, // Setze auf `false` in Produktionsumgebungen
             }),
             inject: [ConfigService],
         }),
